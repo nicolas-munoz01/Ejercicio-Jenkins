e79b287e936382aded93aeaac3dcdfd456cde4c0
nicolas-munoz01 <98959106+nicolas-munoz01@users.noreply.github.com>
2022-02-08 10:03:38 -0500

    Adding message

---
+++ src/Mensaje.java
@@ -0,0 +1,8 @@
+public class Mensaje {
+
+    public static void main(String[] args) {
+
+        System.out.println("Este es el mensaje del modulo git");
+        System.out.println("Cada que se realice un cambio este mensaje se mostrara");
+    }
+}
